37d1cfbcea38a3c072acce4a34110d3906478078
Mirko Tamburini <mirko.tamburini8@gmail.com>
2026-04-21 20:08:34 +0200

    Add files via upload

--- IntegrAutocorrTime.py
+++ IntegrAutocorrTime.py
@@ -1,44 +1,54 @@
 
-# ---------------------------------------------------------------
-#           INTEGRATED AUTOCORRELATION TIME
-# ---------------------------------------------------------------
+# ----------------------------------------------------
+#           AUTOCORRELATION FUNCTION
+#                    AND
+#        INTEGRATED AUTOCORRELATION TIME
+# ----------------------------------------------------
 
 import numpy as np
 
-def autocorr_func(data, size, num_lags):
+def autocorr_func(data, lags):
       """
-      Compute the autocorrelation function and 
-      the integrated autocorrelation time among data 
+      Compute the autocorrelation function of a dataset 
 
       Parameters:
             data (a Numpy array): the original dataset.
-            size (int): the length of the dataset.
-            num_lags (int): the number of autocorrelation lags.
+            lags (int): the number of autocorrelation lags.
 
       Returns:
             corr (a Numpy array): the autocorrelation function
-            tau_int (a Numpy array): the integrated autocorrelation time
       """
 
-      # Compute the mean value of the dataset
-      mean = np.mean(data)
+      data = np.asarray(data)
+      n = len(data)
 
-      # Compute the sample variance (with Bessel's correction 
-      # (N - 1) in denominator -> ddof = 1)
-      sigma_squared = np.var(data, ddof=1)
+      mu = np.mean(data)
+      data_centered = data - mu
 
-      # Create the autocorrelation function and integrated autocorrelation time 
-      corr = np.zeros(num_lags)
-      tau_int = np.zeros(num_lags)
+      sigma2 = np.var(data, ddof=1)
+      corr = np.zeros(lags)
 
-      for k in range(num_lags):
-            for i in range(size - k):
-                  corr[k] += (data[i] - mean)*(data[i + k] - mean)
-      
-            corr[k] /= (float(size - k) * sigma_squared)
+      for lag in range(lags):
+            if lag == 0:
+                  corr[lag] = 1.0
+                  continue
 
-      for i in range(num_lags):
-            for j in range (i):
-                  tau_int[i] += corr[j]
+            c = np.dot(data_centered[:n-lag], data_centered[lag:])
+            corr[lag] = c / ((n - lag) * sigma2)
 
-      return corr, tau_int
+      return corr
+
+def int_autocorr_time(corr):
+      """
+      Compute the integrated autocorrelation time of a dataset 
+
+      Parameters:
+            corr (a Numpy array): the autocorrelation function
+
+      Returns:
+            t_int (a Numpy array): the integrated autocorrelation time
+      """
+      t_int = np.zeros_like(corr)
+      t_int[0] = 0.5
+      t_int[1:] = 0.5 + np.cumsum(corr[1:])
+      return t_int
